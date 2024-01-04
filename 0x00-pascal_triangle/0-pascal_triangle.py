2d6cbf0b971ee50ef90fadf7cb8f62a20d6b84be
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 20:59:26 +0300

    create pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n: int):
+    """ function to create the pascal triangle
+        Args:
+            n (_type_): number of rows
+        Returns: an array of integer
+    """
+    triangle = []
+    for x in range(n):
+        tmp = []
+        if x == 0:
+            tmp.append(1)
+        elif x == 1:
+            tmp = triangle[0].copy()
+            tmp.append(1)
+        else:
+            prev = triangle[x - 1]
+            for i in range(len(prev) - 1):
+                two_sum = prev[i] + prev[i + 1]
+                tmp.append(two_sum)
+            tmp.insert(0, 1)
+            tmp.append(1)
+        triangle.append(tmp)
+
+    return triangle
