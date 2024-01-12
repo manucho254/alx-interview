c88e63bf6ce3ec7062069997e83888ecb0b530e6
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-12 04:52:14 +0300

    code fixes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -19,16 +19,18 @@ def canUnlockAll(boxes: List[List]) -> bool:
         return True
 
     available_keys = {0: 0}
+    all_boxes = [x for x in range(len(boxes))]
     # add all keys found in first box to available keys
     for x in boxes[0]:
-        available_keys[x] = x
+        if x in all_boxes:
+            available_keys[x] = x
 
     count = 20
     while count > 0:
         for box in range(1, len(boxes)):
             if box in available_keys:
                 for k in boxes[box]:
-                    if k in range(len(boxes)):
+                    if k in all_boxes:
                         available_keys[k] = k
         count -= 1
 
