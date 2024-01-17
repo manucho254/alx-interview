69b78dab00447ea372db8ced78f6a4eb08a08068
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-17 17:19:12 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" solving minimum operations problem
+"""
+from typing import List
+
+
+def minOperations(n: int) -> int:
+    """
+    Args:
+        n (_type_): _description_
+    Return:
+        sum of all prime factors
+    """
+    if n <= 1:
+        return 0
+
+    operations: List = []
+    divisor: int = 2
+
+    while divisor <= n:
+        if n % divisor == 0:
+            operations.append(divisor)
+            n = n / divisor
+        else:
+            divisor += 1
+
+    return sum(operations)
