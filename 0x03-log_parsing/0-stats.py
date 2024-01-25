f1102d7b603c24d92f9e158028802fb2feb5f3ba
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-26 02:17:30 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,23 +2,12 @@
 """ Log parsing module
 """
 import sys
-import re
-
 
 status_codes = {}
 possible_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 line_count = 0
 total_size = 0
-
-
-def valid_format(line):
-    """checks if the line have a valid format"""
-    pattern = (
-        r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - "
-        r'\[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-    )
-    match = re.match(pattern, line)
-    return bool(match)
+status_code = 0
 
 
 def print_status(codes, total):
@@ -34,30 +23,23 @@ def print_status(codes, total):
 
 try:
     for line in sys.stdin:
-        if not valid_format(valid_format):
-            continue
-
         split_line = line.split(" ")
-        status_code = 0
-        file_size = 0
+        line_count += 1
 
         try:
             status_code = int(split_line[-2])
-            file_size = int(split_line[-1])
+            total_size += int(split_line[-1])
         except Exception as e:
             continue
 
         if status_code in possible_codes:
-            if status_codes.get(status_code) is not None:
+            if status_codes.get(status_code):
                 status_codes[status_code] += 1
             else:
                 status_codes[status_code] = 1
 
-        if line_count % 10 == 0 and line_count >= 10:
+        if line_count % 10 == 0:
             print_status(status_codes, total_size)
-
-        total_size += file_size
-        line_count += 1
 
 except KeyboardInterrupt as e:
     print_status(status_codes, total_size)
