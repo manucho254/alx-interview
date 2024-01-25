2b28d7b9348b3b92cf2adf7276da7c44c8ef2f3a
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-26 01:48:23 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,38 +4,41 @@
 import sys
 
 
-def print_status(status_codes, total_size):
-    """print status codes"""
-    print("File size: {}".format(total_size))
-    for key, val in status_codes.items():
-        print("{}: {}".format(key, val))
-
-
 status_codes = {}
 possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
 line_count = 0
 total_size = 0
 
 
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            split_line = line.split(" ")
-            status_code: str = split_line[-2]
-            file_size = split_line[-1]
+def print_status(codes, total):
+    """print status codes
+    Args:
+        codes: status codes dict
+        total: total size
+    """
+    print("File size: {}".format(total))
+    for key, val in codes.items():
+        print("{}: {}".format(key, val))
 
-            if status_code.isnumeric() and status_code in possible_codes:
-                if status_codes.get(status_code) is not None:
-                    status_codes[status_code] += 1
-                else:
-                    status_codes[status_code] = 1
 
-            if line_count % 10 == 0 and line_count >= 10:
-                print_status(status_codes, total_size)
+try:
+    for line in sys.stdin:
+        split_line = line.split(" ")
+        status_code: str = split_line[-2]
+        file_size = split_line[-1]
 
-            total_size += int(file_size)
-            line_count += 1
+        if status_code.isnumeric() and status_code in possible_codes:
+            if status_codes.get(status_code) is not None:
+                status_codes[status_code] += 1
+            else:
+                status_codes[status_code] = 1
 
-    except KeyboardInterrupt as e:
-        print_status(status_codes, total_size)
-        raise e
+        if line_count % 10 == 0 and line_count >= 10:
+            print_status(status_codes, total_size)
+
+        total_size += int(file_size)
+        line_count += 1
+
+except KeyboardInterrupt as e:
+    print_status(status_codes, total_size)
+    raise e
