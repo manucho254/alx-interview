e15b855cc264060148c8bdaf04e97083c42c43d5
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-29 19:10:46 +0300

    fixes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,6 +3,9 @@
 
 def makeChange(coins, total):
     """making change problem"""
+    if total <= 0:
+        return 0
+
     dp = [0] + [float("inf")] * total
     for i in range(1, total + 1):
         for j in coins:
