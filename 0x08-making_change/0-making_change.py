443c406e8cf5da5038c668b5ea313af5d92ef021
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-29 15:54:58 +0300

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total) -> int:
+    """Get minimum number of coins
+
+    Args:
+        coins (_type_): coins array
+        total (_type_): total amount
+
+    Returns:
+        int: minimum number of coins if found else -1
+    """
+    if total <= 0:
+        return 0
+
+    cache = {}
+
+    def dp(curAmt):
+        if curAmt in cache:
+            return cache[curAmt]
+        elif curAmt > total:
+            return float("inf")
+        elif curAmt == total:
+            return 0
+
+        cache[curAmt] = min(dp(curAmt + x) + 1 for x in coins)
+        return cache[curAmt]
+
+    ans = dp(0)
+    return ans if ans != float("inf") else -1
