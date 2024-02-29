0369ff30b3559bb5145a98c00d1f1b96886ffdef
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-29 19:19:40 +0300

    fixes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,7 +13,7 @@ def makeChange(coins, total):
 
     for i in range(1, total + 1):
         for j in coins:
-            if i >= j:
+            if i - j >= 0:
                 dp[i] = min(dp[i - j] + 1, dp[i])
 
     return dp[total] if dp[total] != total + 1 else -1
