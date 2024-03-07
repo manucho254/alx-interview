b037d60c53fca85b6b2c25dd495771adbe057dff
manucho254 <manuchoadero@gmail.com>
2024-03-07 19:00:26 +0000

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -37,11 +37,11 @@ def island_perimeter(grid):
             for dr, dc in directions:
                 r, c = row + dr, col + dc
                 if (
-                    r in range(rows)
-                    and c in range(cols)
-                    and grid[r][c] == 1
-                    and (r, c) not in visited
-                ):
+                        r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c] == 1 and
+                        (r, c) not in visited
+                        ):
                     q.append((r, c))
                     visited.add((r, c))
 
