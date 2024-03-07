1e3b58036714aa636ce10bb130ad152a20862a7e
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-07 22:05:15 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -29,7 +29,7 @@ def island_perimeter(grid):
         q.append((r, c))
 
         while q:
-            row, col = q.pop(0)
+            row, col = q.pop()
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
             for dr, dc in directions:
